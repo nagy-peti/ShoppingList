c228c00d21da4394b3c71e78a0b6f12cf8d015e9
Pinter Gergo <gergo3.14nter@gmail.com>
2020-11-15 18:08:02 +0100

    created routing, initialized page components

--- Angular/static-ui/src/app/app.module.ts
+++ Angular/static-ui/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SocialComponent } from './social/social.component';
+import { MainComponent } from './main/main.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ModifyComponent } from './modify/modify.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: LoginComponent},
+      {path: 'main', component: MainComponent},
+      {path: 'social', component: SocialComponent},
+      {path: 'recipes', component: RecipesComponent},
+      {path: 'modify', component: ModifyComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]

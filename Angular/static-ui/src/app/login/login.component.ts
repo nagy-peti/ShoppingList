3896b4ee2dc6cefb52cccc961024e14fa3ec4963
Pinter Gergo <gergo3.14nter@gmail.com>
2020-11-19 18:07:00 +0100

    added login page

--- Angular/static-ui/src/app/login/login.component.ts
+++ Angular/static-ui/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginGuard } from '../login-guard/login.guard';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public password: string = "";
+  public username: string = "";
+  public hasError: boolean = false;
+  public isInvalid: boolean = false;
+  
+  private fixPassword = 'temp';
+  private fixUsername = 'temp';
+
+  constructor(
+    private router:Router,
+    private login: LoginGuard,
+    ) { }
 
   ngOnInit(): void {
   }
 
+  getVerification(){
+    if(this.username === this.fixUsername && this.password === this.fixPassword){
+          this.login.changeLogInState(true);
+          this.router.navigate(['/main']);
+          this.isInvalid = false;
+        }else{
+          this.isInvalid = true;
+          this.username = "";
+          this.password = "";
+        };
+  };
 }

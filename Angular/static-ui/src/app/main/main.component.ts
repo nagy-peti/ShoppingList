59574928ba87fbfabe691101d94810ae5693607c
Pinter Gergo <gergo3.14nter@gmail.com>
2021-01-03 11:27:15 +0100

    changed main page to work with dynamic data

--- Angular/static-ui/src/app/main/main.component.ts
+++ Angular/static-ui/src/app/main/main.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import * as _ from 'lodash';
 import { Subscription } from 'rxjs';
-import { ValidationService } from '../services/validation.service';
+import { UsersService } from '../services/users.service';
 import { SerialNumberService } from '../services/serialNumber.service';
+import { ItemService } from '../services/item.service';
+import { ShoppingListService } from '../services/shopping-list.service';
 
 interface ItemList{
   [key:string]:ItemTuple[];
 }
-interface ItemTuple{
-  name: string;
-  qty: string;
-  qtyType: string;
+
+interface Map{
+  [key:string]:number;
 }
 
-interface ListRow{
-  id: number;
-  items: any[]; //later itemtuple
+export interface ItemTuple{
+  shopping_list_id: number;
+  id?:number;
+  name: string;
+  quantity: string;
+  quantity_type: string;
+}
+
+export interface ListRow{
+  id?: number;
+  items: any[];
   name: string;
   owner_id: number;
-  recpies: any[]; //change later
-  shared_with_friends: string;
+  recpies?: any[];
+  shared_with_friends?: string;
 }
 
 @Component({
@@ -44,9 +53,12 @@ export class MainComponent implements OnInit, OnDestroy {
   public items: ItemList = {};
   private subscription!: Subscription;
   private userId!: number;
+  private shoppingListIds:Map = {};
 
   constructor(
-    private backend: ValidationService,
+    private usersService: UsersService,
+    private itemService: ItemService,
+    private shoppingListService: ShoppingListService,
     private serialNumber: SerialNumberService
   ) { }
 
@@ -60,23 +72,24 @@ export class MainComponent implements OnInit, OnDestroy {
       data => {
         this.userId = data;
         this.getShoppingLists();
-        console.log(this.userId);
       }
     )
   }
 
   private getShoppingLists(){
-    this.backend.getShoppingLists(this.userId).subscribe(
+    this.usersService.getShoppingLists(this.userId).subscribe(
       (data) => {
-        console.log(data);
         (<ListRow[]>data).forEach((element:ListRow) => {
-          this.items[element.name] = element.items.map(item => {
-            return {
+          this.items[element.name] = <any[]>element.items.map(item => {
+            return <ItemTuple>{
+              shopping_list_id: item.shopping_list_id,
+              id: item.id,
               name: item.name,
-              qty: item.quantity,
-              qtyType: item.quantity_type
+              quantity: item.quantity,
+              quantity_type: item.quantity_type
             }
           })
+          this.shoppingListIds[element.name] = <number>element.id;
         })
         this.chosenTuple = this.items[Object.keys(this.items)[0]];
         this.chosenTupleName = Object.keys(this.items)[0];
@@ -86,7 +99,6 @@ export class MainComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(){
-    console.log('destroyed');
     this.subscription.unsubscribe();
   }
 
@@ -97,49 +109,75 @@ export class MainComponent implements OnInit, OnDestroy {
   }
 
   addItem(){
-    this.items[this.chosenTupleName].push({
-      name: this.itemName, 
-      qty: this.qty,
-      qtyType: this.qtyType
-    });
-    this.chosenTuple = _.cloneDeep(this.items[this.chosenTupleName]);
+    let itemToAdd = <ItemTuple>{
+      shopping_list_id: this.shoppingListIds[this.chosenTupleName],
+      name: this.itemName,
+      quantity: this.qty,
+      quantity_type: this.qtyType
+    };
+    this.itemService.addItem(itemToAdd).subscribe(
+      data => {
+        itemToAdd.id = (<any>data).id;
+        this.items[this.chosenTupleName].push(itemToAdd);
+        this.chosenTuple = _.cloneDeep(this.items[this.chosenTupleName]);
+      }
+    );
     this.itemName = "";
     this.qty = "";
     this.qtyType = "";
   }
 
   deleteRow(data:ItemTuple){
-    let index = this.items[this.chosenTupleName].findIndex(item => 
-      item.name === data.name && item.qty === data.qty
-    );
-    this.items[this.chosenTupleName].splice(index,1);
-    this.chosenTuple = _.cloneDeep(this.items[this.chosenTupleName]);
+    this.itemService.deleteItem(<number>data.id).subscribe(
+      () => {
+        let index = this.items[this.chosenTupleName].findIndex(item => 
+          item.name === data.name && item.quantity === data.quantity
+        );
+        this.items[this.chosenTupleName].splice(index,1);
+        this.chosenTuple = _.cloneDeep(this.items[this.chosenTupleName]);
+      }
+    )
   }
 
   deleteList(data:string){
     if(this.editMode){
-      delete this.items[data];
-      this.listTitles = Object.keys(this.items);
-      if(this.chosenTupleName === data){
-        this.chosenTuple = this.items[Object.keys(this.items)[0]];
-        this.chosenTupleName = Object.keys(this.items)[0];
-      }
+      this.shoppingListService.deleteList(this.shoppingListIds[data]).subscribe(
+        msg => {
+          delete this.items[data];
+          this.listTitles = Object.keys(this.items);
+          if(this.chosenTupleName === data){
+            this.chosenTuple = this.items[Object.keys(this.items)[0]];
+            this.chosenTupleName = Object.keys(this.items)[0];
+          }
+        }
+      )
+
     }
   }
 
   addNewList(){
-    this.items[this.listName]=<ItemTuple[]>[];
-    this.chosenTuple = this.items[this.listName];
-    this.chosenTupleName = this.listName;
-    this.listTitles = _.cloneDeep(Object.keys(this.items));
-    this.listName= "";
+    let rowToAdd = <ListRow>{
+      id: undefined,
+      items: [],
+      name: this.listName,
+      owner_id: this.userId,
+    }
+    this.shoppingListService.addRow(rowToAdd).subscribe(
+      data => {
+        this.shoppingListIds[this.listName] = (<any>data).id
+        this.items[this.listName]=<ItemTuple[]>[];
+        this.chosenTuple = this.items[this.listName];
+        this.chosenTupleName = this.listName;
+        this.listTitles = _.cloneDeep(Object.keys(this.items));
+        this.listName= "";
+      }
+    )
   }
 
   modifyRow(data:ItemTuple){
     this.itemName = data.name;
-    this.qty = data.qty;
-    this.qtyType = data.qtyType;
+    this.qty = data.quantity;
+    this.qtyType = data.quantity_type;
     this.deleteRow(data);
   }
-
 }

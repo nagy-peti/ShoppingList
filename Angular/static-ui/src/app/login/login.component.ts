e9cc77cee858864c0b076e80950a348c82905b38
Pinter Gergo <gergo3.14nter@gmail.com>
2021-01-03 11:27:51 +0100

    added backend verification and register functions to login, register

--- Angular/static-ui/src/app/login/login.component.ts
+++ Angular/static-ui/src/app/login/login.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 import { LoginGuard } from '../login-guard/login.guard';
 import { MatDialog } from '@angular/material/dialog';
 import { RegisterDialogComponent } from './register-dialog/register-dialog.component';
+import { UsersService } from '../services/users.service';
+import * as _ from 'lodash';
+import { SerialNumberService } from '../services/serialNumber.service';
 
 @Component({
   selector: 'app-login',
@@ -15,21 +18,23 @@ export class LoginComponent implements OnInit {
   public username: string = "";
   public hasError: boolean = false;
   public isInvalid: boolean = false;
-  
-  private fixPassword = 'temp';
-  private fixUsername = 'temp';
 
   constructor(
     private router:Router,
     private login: LoginGuard,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private backend: UsersService,
+    private serialNumber: SerialNumberService
     ) { }
 
   ngOnInit(): void {
   }
 
   getVerification(){
-    if(this.username === this.fixUsername && this.password === this.fixPassword){
+    this.backend.getVerification(_.clone(this.username), _.clone(this.password)).subscribe(
+      data => {
+        if(!_.isNull(data)){
+          this.serialNumber.broadcastId(<number>data);
           this.login.changeLogInState(true);
           this.router.navigate(['/main']);
           this.isInvalid = false;
@@ -38,6 +43,8 @@ export class LoginComponent implements OnInit {
           this.username = "";
           this.password = "";
         };
+      }
+    );
   };
 
   openDialog(){
--- Angular/static-ui/src/app/login/register-dialog/register-dialog.component.ts
+++ Angular/static-ui/src/app/login/register-dialog/register-dialog.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { LoginGuard } from 'src/app/login-guard/login.guard';
+import { SerialNumberService } from 'src/app/services/serialNumber.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-register-dialog',
@@ -12,19 +14,34 @@ export class RegisterDialogComponent implements OnInit {
 
   public username: string = "";
   public password: string = "";
-  public email: string = "";
+
+  public isInvalid: boolean = false;
   constructor(
     private router: Router,
     private login: LoginGuard,
-    private dialogRef: MatDialogRef<RegisterDialogComponent>
+    private dialogRef: MatDialogRef<RegisterDialogComponent>,
+    private backend: UsersService,
+    private serialNumber: SerialNumberService
   ) { }
 
   ngOnInit(): void {
   }
 
   createAccount(){
-    this.login.changeLogInState(true);
-    this.router.navigate(['/main']);
-    this.dialogRef.close();
+    this.backend.addNewUser(this.username, this.password).subscribe(
+      data => {
+        if(<number>data === -1){
+          this.isInvalid = true;
+          this.username = "";
+          this.password = "";
+        }else{
+          this.isInvalid = false;
+          this.serialNumber.broadcastId(<number>data);
+          this.login.changeLogInState(true);
+          this.router.navigate(['/main']);
+          this.dialogRef.close();
+        }
+      }
+    )
   }
 }
